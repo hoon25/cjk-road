72125e94454468fc07e0859d2199584bc8936c7a
markchck <markchck@gmail.com>
2022-09-20 08:29:12 +0900

    이메일, 이름, 닉네임 create api 완성

--- web/app.py
+++ web/app.py
@@ -1,4 +1,10 @@
+from crypt import methods
+import json
 from flask import Flask, render_template, request, jsonify
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.web
 
 app = Flask(__name__)
 
@@ -6,6 +12,22 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
+@app.route('/user_list', methods=["GET"])
+def login():
+    user_list = list(db.web.find({}, {'_id': 0 }))
+    return jsonify({'result':'success', 'msg':'Connected', 'data': user_list})
+
+@app.route('/register', methods=['POST'])
+def register_api():
+    name = request.form["name"]
+    nickname = request.form["nickname"]
+    email = request.form["email"]
+    user_info = {"name": name, "nickname": nickname, "email": email}
+    db.web.insert_one(user_info)
+    return jsonify ({"result":"success"})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
+ 
