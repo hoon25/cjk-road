fe92e6431850b101321aa0f00df56b93f5c2eb3d
hoon25 <cchoon95@hanmail.net>
2022-09-20 15:17:52 +0900

    feat/#5 : 식당이름,별점,링크 크롤링 기능구현

---
+++ batch/N_Map_Crawling/restraunt_crawling.py
@@ -0,0 +1,95 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from urllib import parse
+from batch.common.custom_logger import get_custom_logger
+import sys
+
+logger = get_custom_logger("crawl")
+driver = webdriver.Chrome()
+
+
+def main(url, key_word):
+    try:
+        start_time = time.time()
+        logger.info(f"검색 키워드 : {key_word}")
+        driver.get(url)
+        time_wait(10, 'div.input_box > input.input_search')
+
+        search = driver.find_element(By.CSS_SELECTOR, 'div.input_box > input.input_search')
+        search.send_keys(key_word)
+        search.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+        switch_frame('searchIframe')
+        page_down(40)
+        time.sleep(1)
+
+        # 매장 리스트
+        store_master = driver.find_elements(By.CSS_SELECTOR, 'div.Ryr1F > ul > li')
+        store_dict_list = []
+
+        logger.info(f"결과 개수  : {len(store_master)}건")
+
+        for store in store_master:
+            logger.debug("---------------------------------------------------")
+            store_name, store_star, store_link = '', '', ''
+            store_dict = {}
+            logger.debug("---------------------이름")
+            store_name = store.find_element(By.CSS_SELECTOR, 'div.CHC5F > a > div > div > span.place_bluelink.TYaxT').text
+            logger.debug(store_name)
+            logger.debug("---------------------별점")
+            try:
+                store_star = store.find_element(By.CSS_SELECTOR, 'div.CHC5F > div.Dr_06 > span.h69bs.a2RFq > em').text
+            except:
+                store_star = "0.0"
+            logger.debug(store_star)
+            logger.debug("---------------------링크")
+            store_link = f"https://map.naver.com/v5/search/{parse.quote(store_name)}/"
+            logger.debug(store_link)
+
+            store_dict = {'store_name': store_name, 'store_star': store_star, 'store_link': store_link}
+            store_dict_list.append(store_dict)
+        return store_dict_list
+    except:
+        logger.exception(f"{key_word} 크롤링 실패")
+    finally:
+        driver.quit()
+        logger.info(f"소요시간:{time.time() - start_time}")
+
+
+# 페이지 로드 대기
+def time_wait(num, code):
+    try:
+        wait = WebDriverWait(driver, num).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, code)))
+    except:
+        logger.exception(code, '태그를 찾지 못하였습니다.')
+        driver.quit()
+    return wait
+
+
+# 프레임 변경
+def switch_frame(frame):
+    driver.switch_to.default_content()
+    driver.switch_to.frame(frame)
+
+
+# 페이지 내리기
+def page_down(num):
+    body = driver.find_element(By.CSS_SELECTOR, 'body')
+    body.click()
+    for i in range(num):
+        body.send_keys(Keys.PAGE_DOWN)
+
+
+# 메인 함수
+if __name__ == "__main__":
+    url = "https://map.naver.com/v5/search"
+    key_word = sys.argv[1]
+
+    main(url, key_word)
+
