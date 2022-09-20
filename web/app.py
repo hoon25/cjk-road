16b5c7e38bbbb4bf020d7c285bd4bf300ca3c9b7
narin <nalynnshinhwa1128@gmail.com>
2022-09-20 15:51:53 +0900

    로그인/회원가입 페이지 구현

--- web/app.py
+++ web/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('login.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5500, debug=True)
