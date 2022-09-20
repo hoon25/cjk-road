67da520c62d7244f7906e38a8c98bc9a4ec86dcd
narin <nalynnshinhwa1128@gmail.com>
2022-09-20 20:52:20 +0900

    module 에러 수정

--- web/app.py
+++ web/app.py
@@ -1,14 +1,17 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from flask import Flask, render_template, request, jsonify, flash
-from web.common.mongo_connector import get_db, get_conn_with_collections
-from web.common.response_factory import get_success_json, get_failure_json
-from web.common.custom_logger import get_custom_logger
+# from web.common.mongo_connector import get_db, get_conn_with_collections
+# from web.common.response_factory import get_success_json, get_failure_json
+# from web.common.custom_logger import get_custom_logger
+from common import mongo_connector, custom_logger, response_factory
 
 app = Flask(__name__)
 
-db = get_db()
-logger = get_custom_logger("web")
+db = mongo_connector.get_db()
+logger = custom_logger.get_custom_logger("web")
 
-@app.route('/')
+@app.route('/') 
 def home():
    return render_template('login.html')
 
@@ -18,13 +21,13 @@ def restaurant_get(search_univ):
         rest_list = list(db.restaurant.find({"university_name": search_univ}))
         for rest in rest_list:
             rest['_id'] = str(rest['_id'])
-        result = get_success_json("검색 성공", rest_list)
+        result = response_factory.get_success_json("검색 성공", rest_list)
         logger.info(result)
         return render_template('cards.html', result = result)
 
     except:
         flash("등록되지않은 대학교입니다.")
-        return render_template('cards.html', result = get_failure_json("검색 실패"))
+        return render_template('cards.html', result = response_factory.get_failure_json("검색 실패"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5500, debug=True)
