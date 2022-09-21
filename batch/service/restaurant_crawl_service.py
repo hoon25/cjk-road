fa4b8c27d6ee565b4f87eb58e964a2c46ff4067d
hoon25 <cchoon95@hanmail.net>
2022-09-21 16:20:57 +0900

    feat/#5 대학리스트추가

--- batch/service/restaurant_crawl_service.py
+++ batch/service/restaurant_crawl_service.py
@@ -4,7 +4,8 @@ import time
 
 
 def main():
-    univ_list = ["서울대학교맛집", "고려대학교맛집", "연세대학교맛집"]
+    # univ_list = ["서울대학교맛집", "고려대학교맛집", "연세대학교맛집"]
+    univ_list = ["세종대학교맛집", "동국대학교맛집", "서강대학교맛집", "한양대학교맛집", "카이스트맛집", "제주대학교맛집"]
     for univ in univ_list:
         data = restaurant_crawling.main(univ)
         save_data("restaurant", data)
