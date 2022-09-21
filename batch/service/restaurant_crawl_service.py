fb6d53e2c60c7ccf33c0560cacd655155b59db6a
hoon25 <cchoon95@hanmail.net>
2022-09-21 15:57:54 +0900

    chore Mongo IP변경

--- batch/service/restaurant_crawl_service.py
+++ batch/service/restaurant_crawl_service.py
@@ -1,13 +1,17 @@
 from batch.common.mongoConnector import save_data
 from batch.N_Map_Crawling import restaurant_crawling
-import sys
+import time
 
 
 def main():
     univ_list = ["서울대학교맛집", "고려대학교맛집", "연세대학교맛집"]
-    keyword = "연세대학교맛집"
-    data = restaurant_crawling.main(keyword)
-    save_data("restaurant", data)
+    for univ in univ_list:
+        data = restaurant_crawling.main(univ)
+        save_data("restaurant", data)
+        time.sleep(1)
+    # keyword = "연세대학교맛집"
+    # data = restaurant_crawling.main(keyword)
+    # save_data("restaurant", data)
 
 
 if __name__ == "__main__":
